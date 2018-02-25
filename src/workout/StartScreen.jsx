e194733d13d8cad4437803cc0b046fdec144219e
Tomás Cañibano <tomas.cani@gmail.com>
2018-02-25 12:43:25 -0300

    Use Material UI input

--- src/workout/StartScreen.jsx
+++ src/workout/StartScreen.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import Screen from './Screen';
-import Input from '../shared/Input';
 
 class StartScreen extends React.Component {
   constructor(props) {
@@ -11,23 +11,21 @@ class StartScreen extends React.Component {
     this.handleInput = this.handleInput.bind(this);
   }
 
-  handleInput(value) {
-    this.props.handleConfig(value);
+  handleInput(e) {
+    this.props.handleConfig(e.target.value);
   }
 
   render() {
     return (
       <Screen>
         <p>Configure workout</p>
-        <label htmlFor="max-attempts">
-          # Attempts
-          <Input
-            id="max-attempts"
-            type="number"
-            onInput={this.handleInput}
-            value={this.props.maxAttempts}
-          />
-        </label>
+        <TextField
+          id="max-attempts"
+          floatingLabelText="# Attempts"
+          type="number"
+          onChange={this.handleInput}
+          value={this.props.maxAttempts}
+        />
         <RaisedButton
           label="Start workout"
           onClick={this.props.onStart}
