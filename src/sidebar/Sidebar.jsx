2e99eb168baecc63a2fb0aecdd5586d60a8e5fbe
Tomás Cañibano <tomas.cani@gmail.com>
2018-04-15 19:58:50 -0300

    Remove astronomy

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import chemistry from './icons/chemistry.svg';
 import geography from './icons/earth-globe.svg';
-import astronomy from './icons/telescope.svg';
 import Icon from './Icon';
 import './Sidebar.css';
 
 const iconsData = [
-  { url: '/astronomy', src: astronomy, name: 'Astronomy' },
   { url: '/chemistry', src: chemistry, name: 'Chemistry' },
   { url: '/geography', src: geography, name: 'Geography' },
 ];
